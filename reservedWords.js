de214f308e223376ec56e5bf59cbddf0ddd46427
xkmmmm <xu.keme@husky.neu.edu>
2020-05-06 22:17:41 -0700

    delete trash code

--- reservedWords.js
+++ reservedWords.js
@@ -1,10 +1,3 @@
-$.ajax({
-    url: "https://dev.mysql.com/doc/refman/8.0/en/keywords.html",
-    dataType: 'jsonp',
-    success: function(data){
-      console.log( data );
-    }
-});
 var list = document.querySelectorAll(".literal")
 var res = new Set();
 list.forEach((ele)=>{
